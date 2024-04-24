2c9c71ed0a33a9acccd18de132cac94105a40eaf
kamilapu <kamilapuzyrova@gmail.com>
2024-04-24 16:48:27 +0100

    add home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Header } from "../../shared/ui/Header/Header";
 
 export const Home = () => {
   return (
+    <>
+    	<Header />
     <div>Home</div>
+    </>
   )
 }
